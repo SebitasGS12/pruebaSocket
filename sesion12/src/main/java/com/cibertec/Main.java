d9c0b24b455293e5c98066fdb817faa50580c9ef
SebitasGS12 <sebasgoy16@gmail.com>
2024-05-23 20:15:38 -0500

    primer commit

---
+++ sesion12/src/main/java/com/cibertec/Main.java
@@ -0,0 +1,9 @@
+package com.cibertec;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! --> cibertec --> goyzueta el hacker");
+
+        
+    }
+}
